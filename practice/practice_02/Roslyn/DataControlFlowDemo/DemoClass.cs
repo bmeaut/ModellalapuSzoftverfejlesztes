be02a75e784c712d4cb29b12c1e5933a3f659c42
gmezei <gmezei@aut.bme.hu>
2023-03-11 16:44:32 +0100

    Added Practice 02 - Roslyn demo projects

---
+++ practice/practice_02/Roslyn/DataControlFlowDemo/DemoClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataControlFlowDemo
+{
+    public class DemoClass
+    {
+        private const int DummyConst = 10;
+
+        public void DataFlowAnalysis()
+        {
+            string x = "5";
+            int y = 10;
+            int z = 25;
+            x = "30";
+            y = 20;
+            y = 50;
+        }
+
+        public int ControlFlowAnalysis()
+        {
+            var random = new Random().Next(1, 100);
+            if (random >= 50)
+            {
+                return 5;
+            }
+
+            if (random >= 75)
+            {
+                return DummyConst;
+            }
+            
+            return random;
+        }
+    }
+}
---
+++ practice/practice_02/Roslyn/ScriptingAPI/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+
+namespace ScriptingAPI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        //static async Task Main(string[] args)
+        {
+            var person = new Person
+            {
+                Name = "Superman",
+                Age = 30
+            };
+
+            Console.WriteLine(person);
+
+            //await Scripting1();
+            //await Scripting2();
+            //await Scripting3();
+            //await Scripting4();
+            //await Scripting5();
+
+            Console.ReadLine();
+        }
+
+        static async Task Scripting1()
+        {
+            var result = await CSharpScript.EvaluateAsync("5+4*2");
+            Console.WriteLine(result);
+        }
+
+        static async Task Scripting2()
+        {
+            while (true)
+            {
+                var codeToEval = Console.ReadLine();
+                var result = await CSharpScript.EvaluateAsync(codeToEval);
+                }
+        }
+
+        static async Task Scripting3()
+        {
+
+            while (true)
+            {
+                    var codeToEval = Console.ReadLine();
+                    var result = await CSharpScript.EvaluateAsync(codeToEval, ScriptOptions.Default.WithImports("System"));
+                    Console.WriteLine(result);
+            }
+        }
+
+        static async Task Scripting4()
+        {
+            while (true)
+            {
+                try
+                {
+
+                    var codeToEval = Console.ReadLine();
+                    var result = await CSharpScript.EvaluateAsync(codeToEval, ScriptOptions.Default.WithImports("System"));
+                    Console.WriteLine(result);
+                }
+                catch (CompilationErrorException e)
+                {
+                    Console.WriteLine(string.Join(Environment.NewLine, e.Diagnostics));
+                }
+            }
+        }
+
+        static async Task Scripting5()
+        {
+            var state = await CSharpScript.RunAsync("int a = 1;");
+            state = await state.ContinueWithAsync("int b = 2;");
+            state = await state.ContinueWithAsync("int c = a + b;");
+            state = await state.ContinueWithAsync("c");
+            Console.WriteLine(state.ReturnValue);
+        }
+
+    }
+}
