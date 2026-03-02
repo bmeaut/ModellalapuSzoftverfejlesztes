9c7b999c824b88c2e8b67f245b50a95853ce4635
Fsomogyi <ferencsomogyi92@gmail.com>
2026-03-02 19:00:39 +0100

    Practice 02

---
+++ archive/practices_2025/practice_03/sources/Program.java
@@ -0,0 +1,13 @@
+package statemachines;
+
+public class Program {
+
+	public static void main(String[] args) {
+		var lamp = new Lamp();
+		lamp.Flick();
+		lamp.Flick();
+		lamp.Flick();
+		lamp.Flick();
+	}
+
+}
